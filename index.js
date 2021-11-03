93dd75ccacfae97b941ea210bcb0060825168344
Filipe Lukebana <filipelukebana@gmail.com>
2021-11-03 13:32:49 +0100

    🚀 UPDATE: Get all columns from a chosen table

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ var formStruct = { tables: Array() };
  */
 function getDatabases(_callback) {
     if (auth.env == "mssql") {
-        auth.run("SELECT name FROM master.dbo.sysdatabases", function (results) {
+        auth.run("SELECT name FROM master.dbo.sysdatabases WHERE dbid > 4", function (results) {
             _callback(results.recordset);
         });
     }
@@ -27,17 +27,32 @@ function getDatabases(_callback) {
         });
     }
 }
-function getTablesFromDatabase(table, _callback) {
+/**
+ * Function to fetch all tabales from given database.
+ * @param database The name of the database
+ * @param _callback Callback function that will be fired with the set of tables as argument
+ */
+function getTablesFromDatabase(database, _callback) {
     if (auth.env == "mssql") {
-        auth.run("SELECT table_name FROM [" + table + "].INFORMATION_SCHEMA.TABLES WHERE TABLE_TYPE='BASE TABLE'", function (results) {
+        auth.run("SELECT table_name FROM [" + database + "].INFORMATION_SCHEMA.TABLES WHERE TABLE_TYPE='BASE TABLE'", function (results) {
             _callback(results.recordset);
         });
     }
     else {
-        auth.run("SELECT table_name FROM information_schema.tables WHERE table_schema = " + table, function (error, results) {
+        auth.run("SELECT table_name FROM information_schema.tables WHERE table_schema = " + database, function (error, results) {
             _callback(results);
         });
     }
+}
+/**
+ * Get list of columns from table
+ * @param table
+ * @param _callback
+ */
+function getColumnsFromTables(table, _callback) {
+    auth.run("SELECT COLUMN_NAME FROM INFORMATION_SCHEMA.COLUMNS WHERE TABLE_NAME = '" + table + "'", function (results) {
+        _callback(results);
+    });
 }
 if (auth.env != 'mssql') {
     //Get list of tables from database
@@ -63,7 +78,17 @@ setTimeout(function () {
     getDatabases(function (databases) {
         var _loop_1 = function (index) {
             getTablesFromDatabase(databases[index].name, function (tables) {
-                console.log({ "Database": databases[index].name, tables: tables });
+                var res = { "Database": databases[index].name, tables: tables };
+                var _loop_2 = function (e) {
+                    getColumnsFromTables(tables[e].table_name, function (columns) {
+                        res.tables[e]["columns"] = __spreadArrays(columns.recordset);
+                        if (e == tables.length - 1)
+                            console.log(JSON.stringify(res));
+                    });
+                };
+                for (var e = 0; e < tables.length; e++) {
+                    _loop_2(e);
+                }
             });
         };
         for (var index = 0; index < databases.length; index++) {
--- index.ts
+++ index.ts
@@ -3,10 +3,8 @@ import SQL from "./gnfsql";
 // var auth = new SQL({host:"127.0.0.1",user:"root",database:"angola"})
 var auth = new SQL({env:"mssql",host:"LLDESWKSHP0421",user:"sa",password:"abc@123",database:"REALHOPE"})
 
-
  
 var formStruct = {tables:Array<Object>()};
-
 
 /**
  * Get list of databases on the connection
@@ -14,7 +12,7 @@ var formStruct = {tables:Array<Object>()};
  */
  function getDatabases(_callback:Function){
     if(auth.env == "mssql"){
-        auth.run("SELECT name FROM master.dbo.sysdatabases",(results)=>{
+        auth.run("SELECT name FROM master.dbo.sysdatabases WHERE dbid > 4",(results)=>{
             _callback(results.recordset);
         })
     }else{
@@ -23,18 +21,33 @@ var formStruct = {tables:Array<Object>()};
         })
     }
 }
-
-
-function getTablesFromDatabase(table:string,_callback:Function){
+/**
+ * Function to fetch all tabales from given database.
+ * @param database The name of the database
+ * @param _callback Callback function that will be fired with the set of tables as argument
+ */
+function getTablesFromDatabase(database:string,_callback:Function){
     if(auth.env == "mssql"){
-        auth.run(`SELECT table_name FROM [${table}].INFORMATION_SCHEMA.TABLES WHERE TABLE_TYPE='BASE TABLE'`,(results)=>{
+        auth.run(`SELECT table_name FROM [${database}].INFORMATION_SCHEMA.TABLES WHERE TABLE_TYPE='BASE TABLE'`,(results)=>{
             _callback(results.recordset);
         })
     }else{
-        auth.run(`SELECT table_name FROM information_schema.tables WHERE table_schema = ${table}`,(error,results)=>{
+        auth.run(`SELECT table_name FROM information_schema.tables WHERE table_schema = ${database}`,(error,results)=>{
             _callback(results);
         })
     }
+}
+
+/**
+ * Get list of columns from table
+ * @param table 
+ * @param _callback 
+ */
+function getColumnsFromTables(table:string,_callback:Function){
+        auth.run(`SELECT COLUMN_NAME FROM INFORMATION_SCHEMA.COLUMNS WHERE TABLE_NAME = '${table}'`,(results)=>{
+            _callback(results);
+        })
+    
 }
 
 
@@ -66,12 +79,29 @@ if(auth.env !='mssql'){
 
 setTimeout(() => {
      getDatabases((databases)=>{
-
+       
         for(let index = 0; index < databases.length; index++){
 
             getTablesFromDatabase(databases[index].name,(tables)=>{
-                console.log({"Database":databases[index].name,tables:tables})
+            var res = {"Database":databases[index].name,tables:tables};
+    
+                for(let e = 0; e < tables.length; e++){
+
+                    getColumnsFromTables(tables[e].table_name,(columns)=>{
+                        res.tables[e]["columns"] = [...columns.recordset];
+                        if(e == tables.length-1)
+                        console.log(JSON.stringify(res))
+                    })
+
+                    
+                }
+
+               
+
+                
             })
+
+            
         }
      })
 }, 500);
