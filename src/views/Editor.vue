54a628bf6256cc246e7aead531a670428ef7ffcd
Filipe Lukebana <filipelukebana@gmail.com>
2021-11-04 16:58:00 +0100

    🚀Updates

--- src/views/Editor.vue
+++ src/views/Editor.vue
@@ -9,8 +9,8 @@
             </div>
             <div class="midle flex items-center justify-center normal-case h-full">
 
-                    <button class="btn btn-sm btn-ghost text-white normal-case font-light border-b-4 border-0 h-full border-primary-500 rounded-none"> <luk_row_vertical class="mx-1" /> List </button>
-                    <button class="btn btn-sm btn-ghost text-white normal-case font-light rounded-none"> <luk_add_square class="mx-1" /> Add </button>
+                    <button @click="onList = true" :class="`btn btn-sm btn-ghost text-white normal-case font-light  border-0 h-full hover:border-primary-700 hover:border-b-4  ${onList?'border-primary-500 border-b-4 ':''} rounded-none`"> <luk_row_vertical class="mx-1" /> List </button>
+                    <button @click="onList = false" :class="`btn btn-sm btn-ghost text-white normal-case font-light border-0 h-full hover:border-primary-700 hover:border-b-4 rounded-none ${!onList?'border-primary-500 border-b-4 ':''}`"> <luk_add_square class="mx-1" /> Add </button>
                     
             </div>
             <div class="flex items-center justify-end">
@@ -28,21 +28,22 @@
         </div>
         <div class="body flex h-full w-full bg-gray-100 ">
             <div class="side-menu flex flex-col w-14 h-full bg border-r-2 bg-black_bg-500 border-black_bg-600 ">
-                <button class="btn btn-sm btn-ghost text-white normal-case font-light rounded-none"> <luk_note class="mx-1" /></button>
+                <button class="btn btn-sm btn-ghost text-white normal-case font-light rounded-none border-b border-black_bg-700 py-2 max-h-full h-auto"> <luk_note class="mx-1" /></button>
+                <button class="btn btn-sm btn-ghost text-white normal-case font-light rounded-none border-b border-black_bg-700 py-2 max-h-full h-auto"> <luk_document_code class="mx-1" /></button>
             </div>
             <div class="flex flex-col pages-list w-1/6 h-full ">
                 <div class="btn-group w-full flex ">
-                    <input @click="onList = true" type="radio" name="options" id="option1" data-title="Pages" checked class="btn w-3/6 bg-white text-gray-500 font-light normal-case rounded-none border-0 border-b-2 border-opacity-5"> 
-                    <input @click="onList = false" type="radio" name="options" id="option2" data-title="Components"  class="btn w-3/6 bg-white text-gray-500 font-light normal-case rounded-none border-0 border-b-2 border-opacity-5"> 
+                    <input  type="radio" name="options" id="option1" data-title="Pages" checked class="btn w-3/6 bg-white text-gray-500 font-light normal-case rounded-none border-0 border-b-2 border-opacity-5"> 
+                    <input  type="radio" name="options" id="option2" data-title="Components"  class="btn w-3/6 bg-white text-gray-500 font-light normal-case rounded-none border-0 border-b-2 border-opacity-5"> 
                 </div>
-                <div class="w-full h-full overflow-y-auto pb-16 bg-white  border-r-2 shadow-2xl">
-                    <button v-for="(item, index) in 170" :key="index" class="btn w-full border-0 border-b  border-black_bg-100 btn-ghost text-black_bg-500 normal-case font-light rounded-none  text-left flex justify-start"> <luk_document class="mx-1" /> <p>Page Name {{index}}</p></button>
+                <div class="w-full h-full overflow-y-auto pb-16 bg-white  border-r-2 ">
+                    <button v-for="(item, index) in 20" :key="index" class="btn w-full border-0 border-b  border-black_bg-100 btn-ghost text-black_bg-500 normal-case font-light rounded-none  text-left flex justify-start"> <luk_document class="mx-1" /> <p>Page Name {{index}}</p></button>
                 <div/>
             </div>
            
         </div>
-         <div class=" flex justify-center items-center w-full h-full bg-gray-100 dots" >
-             <div v-if="onList"  class=" flex flex-col page w-5/6 h-full overflow-y-auto bg-white p-10">
+         <div class=" flex justify-center items-center w-full h-full bg-gray-100 dots " >
+             <div v-if="onList"  class="shadow-2xl flex flex-col page w-5/6 h-full overflow-y-auto bg-white p-10">
                 <h1 class="text-4xl mb-8">Teste de listagem</h1>
                 
                 <div class="flex flex-col text-left">
@@ -301,7 +302,63 @@
                     </div>
                     </div>
              </div>
-             <div v-else  class=" flex flex-col page w-5/6 h-full overflow-y-auto bg-white p-10">
+            <div v-else  class="shadow-2xl flex flex-col page w-5/6 h-full overflow-y-auto bg-white p-10">
+
+             <!-- Required form plugin -->
+                <link
+                href="https://cdn.jsdelivr.net/npm/@tailwindcss/custom-forms@0.2.1/dist/custom-forms.css"
+                rel="stylesheet"
+                />
+
+                <div class="p-4 shadow-md rounded-md text-left" style="max-width: 400px">
+                <label class="block">
+                    <span class="text-gray-700">Name</span>
+                    <input class="form-input mt-1 block w-full" placeholder="Jane Doe" />
+                </label>
+
+                <div class="mt-4">
+                    <span class="text-gray-700">Account Type</span>
+                    <div class="mt-2">
+                    <label class="inline-flex items-center">
+                        <input
+                        type="radio"
+                        class="form-radio"
+                        name="accountType"
+                        value="personal"
+                        />
+                        <span class="ml-2">Personal</span>
+                    </label>
+                    <label class="inline-flex items-center ml-6">
+                        <input
+                        type="radio"
+                        class="form-radio"
+                        name="accountType"
+                        value="busines"
+                        />
+                        <span class="ml-2">Business</span>
+                    </label>
+                    </div>
+                </div>
+
+                <label class="block mt-4">
+                    <span class="text-gray-700">Requested Limit</span>
+                    <select class="form-select mt-1 block w-full">
+                    <option>$1,000</option>
+                    <option>$5,000</option>
+                    <option>$10,000</option>
+                    <option>$25,000</option>
+                    </select>
+                </label>
+
+                <div class="flex mt-6">
+                    <label class="flex items-center">
+                    <input type="checkbox" class="form-checkbox" />
+                    <span class="ml-2"
+                        >I agree to the <span class="underline">privacy policy</span></span
+                    >
+                    </label>
+                </div>
+                </div>
              </div>
         </div>
     </div>
@@ -317,6 +374,7 @@ import {
   luk_notification,
   luk_note,
   luk_document,
+  luk_document_code,
 } from "@/assets/iconsax/index.js"; // @ is an alias to /src
 
 @Options({
@@ -328,6 +386,7 @@ import {
     luk_notification,
     luk_note,
     luk_document,
+    luk_document_code,
   },
   data() {
     return {
